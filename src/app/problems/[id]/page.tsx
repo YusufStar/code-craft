4b23c8f7051306385c7095f64d6e5e755857031a
yusuf <ysfyldz0077@gmail.com>
2024-12-15 11:07:21 +0000

    Update page.tsx

--- src/app/problems/[id]/page.tsx
+++ src/app/problems/[id]/page.tsx
@@ -1,8 +1,9 @@
 import Header from "./_components/header";
 import OutputPanel from "./_components/output-panel";
 import EditorPanel from "./_components/editor-panel";
+import { Id } from "../../../../convex/_generated/dataModel";
 
-export default function Home({ params: { id } }: { params: { id: string } }) {
+export default function Home({ params: { id } }: { params: { id: Id<"problems"> } }) {
   return (
     <div className="min-h-screen">
       <div className="max-w-[1800px] mx-auto p-4">
